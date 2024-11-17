9845d3f8d04ffcad1b300ed8e020f9b9a5fa6c88
Mauriciourrozz <9366@holbertonstudents.com>
2024-11-17 18:44:32 -0300

    For of loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [...array]; // Se hace una copia del array para evitar modificar el original
   for (const value of newArray) {
-    newArray[newArray.indexOf(value)] = appendString + value;
+    newArray[value] = appendString + value;
   }
   return newArray;
 }
