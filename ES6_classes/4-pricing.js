4f55625670b09bd81372886f57c0414406359a8c
Mauriciourrozz <9366@holbertonstudents.com>
2024-11-18 12:19:43 -0300

    Class Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from "./3-currency";
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amont = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amont;
+    }
+    set amount(valor) {
+        this._amont = valor;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    set currency(valor) {
+        this._currency = valor;
+    }
+
+    displayFullPrice() {
+        return `${this._amont} ${this._currency._name} (${this._currency._code})`;
+    }
+
+
+    static convertPrice(amount, conversionRate){
+        return amount * conversionRate;
+    }
+}
