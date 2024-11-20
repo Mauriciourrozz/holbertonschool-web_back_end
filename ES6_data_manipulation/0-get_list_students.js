a5795528a6b20f3355335a50cc5e4a490ee07c1e
Mauriciourrozz <9366@holbertonstudents.com>
2024-11-20 13:03:47 -0300

    Return array

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,19 @@
 export default function getListStudents() {
-  const arr = [];
-  const x = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-  const y = {
-    id: 2,
-    firstName: 'Jame',
-    location: 'Columbia',
-  };
-
-  const a = {
-    id: 3,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  arr.push((x), (y), (a));
-  return arr;
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
