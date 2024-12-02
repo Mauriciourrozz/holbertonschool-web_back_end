1e2e273c52c85a40709ebe1fc214220843f7dff5
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-02 16:50:54 -0300

    Return a string of a number float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This function return a string of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function return a string of a float
+    """
+    return str(n)
