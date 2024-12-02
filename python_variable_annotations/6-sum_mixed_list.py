6d25f70b6213a119ff31758f135a8c1aa4e31eea
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-02 17:42:27 -0300

    Sum all elements of a list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,9 +5,11 @@ This file contains a function that sum all elements of a list
 
 
 from typing import List, Union
+
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Sum all elements of a list
-    """    
+    """
     suma = sum(mxd_lst)
     return suma
