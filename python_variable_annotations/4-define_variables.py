3b4545653ba9d3426891d133c33fa72073cf5333
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-02 17:05:46 -0300

    Define differents variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This file annotate differents variables with his annotations
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
