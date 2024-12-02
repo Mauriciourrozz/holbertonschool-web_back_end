d8dee58d72143ec93fe73fce391c10a4b0dcc7c5
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-02 17:16:48 -0300

    Sum all elements of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This file return a sum of all elements of a list
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """
+    This file return a sum of all elements of a list
+    """
+    suma = 0
+    for i in input_list:
+        suma += i
+    return suma
