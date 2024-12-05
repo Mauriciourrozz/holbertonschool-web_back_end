d6dad4ad9d950999c26aa1c1b0c268ae12c31a35
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-05 14:28:45 -0300

    Function asynchronous

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+This file contains a function asynchronous
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10):
+    """
+    Function asynchronous
+    """
     num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
