873c030f4a7e558077e9204fe2e3b76eaae8f51d
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-05 15:59:07 -0300

    Function asynchronous

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,10 +6,10 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Function asynchronous
     """
-    num = random.uniform(0, max_delay)
+    num: float = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
