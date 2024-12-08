62e62bb97cf5d25b586f947fe39964e21e65aec2
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-08 12:45:02 -0300

    Function that return the list of all the delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,8 +3,8 @@
 This file import a function of other file and other elements
 """
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
