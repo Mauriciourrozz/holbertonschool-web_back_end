9f94c03baee106445833fa55bc0c1435b6c391e6
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-09 14:15:20 -0300

    Function async_generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a file that contains a function
+"""
+
+
+import asyncio
+import random
+
+async def async_generator():
+    """
+    Function that does not receive arguments and is responsible for generating
+    a random number between 0 and 10 every one second and returns it
+    """
+    for i in range(11):
+        await asyncio.sleep(1)
+        num: float = random.uniform(0, 10)
+        yield num
