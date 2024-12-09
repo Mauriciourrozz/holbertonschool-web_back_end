41871135f6a590ef6965c07b027b8126101df0bd
Mauriciourrozz <9366@holbertonstudents.com>
2024-12-09 14:21:12 -0300

    Function async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -14,7 +14,7 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Function that does not receive arguments and is responsible for generating
     a random number between 0 and 10 every one second and returns it
     """
-    for i in range(11):
+    for i in range(10):
         await asyncio.sleep(1)
         num = random.uniform(0, 10)
         yield num
