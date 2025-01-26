461e73acdd5036d78676dd7be0dbe0181fa4da79
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-01-26 16:32:02 -0300

    Create a program named 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,9 +6,9 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
- }
+  }
 });
 
 process.stdin.on('end', () => {
- process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
